8e7adeb987fb5a9df3c7727b6ca7ae802fd1af22
Lucas Ericsson <lucasericsson222@gmail.com>
2020-07-06 21:21:03 -0700

    Player Grabbing Asteroids Added
    Note: no releasing of grabbed asteroids yet, signal for release of asteroid just created.

--- Asteroid/Asteroid.gd
+++ Asteroid/Asteroid.gd
@@ -9,7 +9,14 @@ var movement_speed = rand_range(150, 500)
 var start_rot = rand_range(0, 2 * PI)
 var color = Color(1,1,1)
 
+enum states {
+	ROAMING,
+	GRABBED
+}
+var state = states.ROAMING
+var grabbing_player_name = ""
 func _ready():
+	
 	modulate = color
 	randomize()
 	if !is_network_master():
@@ -25,10 +32,20 @@ func _process(delta):
 		rotation = rot
 		position = pos
 	else:
-		rotation += rot_speed * delta
-		var new_movement = Vector2(0,1).rotated(start_rot).normalized() * movement_speed
-		position += new_movement * delta
+		if grabbing_player_name == "":
+			state = states.ROAMING
+		else:
+			state = states.GRABBED
+			
+		if state == states.ROAMING:
+			var new_movement = Vector2(0,1).rotated(start_rot).normalized() * movement_speed
+			position += new_movement * delta
+		elif state == states.GRABBED:
+			var path = "/root/network/" + grabbing_player_name + "/Asteroid_Holding_Position"
+			position = get_node(path).global_position
+			
 		screen_wrap()
+		rotation += rot_speed * delta
 		rset("pos", position)
 		rset("rot", rotation)
 
@@ -37,3 +54,7 @@ onready var screen_size = get_viewport_rect().size
 func screen_wrap():
 	position.x = wrapf(position.x, -50, screen_size.x + 50)
 	position.y = wrapf(position.y, -50, screen_size.y + 50)
+
+
+func _on_not_grabbing(player_name):
+	print("hello")
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,6 @@
 extends Sprite
 
+signal not_grabbing(name)
 onready var screen_size = get_viewport_rect().size
 puppet var pos: Vector2 
 puppet var rot = 0
@@ -16,7 +17,8 @@ func _process(delta):
 	if (name == String(get_tree().get_network_unique_id())):
 		var rot_input = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 		var engine_input = int(Input.is_action_pressed("ui_up"))
-		rpc_id(get_network_master(), "process_input", rot_input, engine_input, delta)
+		var grab_input = Input.is_action_pressed("Grab")
+		rpc_id(get_network_master(), "process_input", rot_input, engine_input,grab_input, delta)
 	if !is_network_master():
 		position = pos
 		rotation = rot
@@ -24,9 +26,11 @@ func _process(delta):
 		rset("pos", position)
 		rset("rot", rotation)
 
-remotesync func process_input(rot_input, engine_input, delta):
+
+remotesync func process_input(rot_input, engine_input, grab_input, delta):
 	if !is_network_master():
 		return
+	$Asteroid_Holding_Position.rotation = rotation
 	rotation += rot_speed * delta * rot_input
 	rset_unreliable("rot", rotation)
 	
@@ -36,8 +40,19 @@ remotesync func process_input(rot_input, engine_input, delta):
 	current_momentum += current_movement * delta
 	position += current_momentum
 	screen_wrap()
-	rset_unreliable("pos", position)
+	rset_unreliable("pos", position)                        
+	if grab_input:
+		var space_state = get_world_2d().direct_space_state
+		var result = space_state.intersect_ray(self.position, Vector2(0,-1000000).rotated(self.rotation), [$KinematicBody2D])
+		if !result.empty():
+			link_asteroid(result)
+	else:
+		emit_signal("not_grabbing", name)
 
 func screen_wrap():
 	position.x = wrapf(position.x, 0, screen_size.x)
 	position.y = wrapf(position.y, 0, screen_size.y)
+
+
+func link_asteroid(result):
+	result["collider"].get_parent().grabbing_player_name = name
